e4e8dbed2b42aa40c8ad715a3e1941789a015334
golu Kumar <golukumarjaisidih@gmail.com>
2024-08-23 00:09:50 +0530

    Corrected name

---
+++ lms-frontend/src/Components/Auth/ReuireAuth.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+function RequireAuth({allowedRoles}){
+ const {isLoggedin,Role} = useSelector((state) => state.auth);      // getting data
+
+ return isLoggedin && allowedRoles.find((myRole) => myRole == role) ? (
+    <Outlet />
+ ) : isLoggedin ?  (<Navigate to = "/denied"/>) : (<Navigate to = "Login"/>)
+}
+
+
+export default RequireAuth;
